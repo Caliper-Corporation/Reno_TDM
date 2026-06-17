9bd405a2b91ffbc54ff21ad96c566daf247fe860
Andrew Rohne <andrew@siliconcreek.net>
2026-06-17 13:38:15 -0400

    Update to gan_loader (fixing minor issue)

--- src/utils/gan_loader.py
+++ src/utils/gan_loader.py
@@ -261,7 +261,6 @@ def main(**kwargs):
         )
         generator.eval()
         with torch.inference_mode():
-        with torch.no_grad():
             gen_util = generator(dv) #.detach().numpy()
         gen_util_out = gen_util.detach().numpy()
         logger.debug("Generator loaded.")
